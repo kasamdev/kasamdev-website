6ca1aaf623124517bff5d08356f0f56de66cdc54
kasamdev <115446471+kasamdev@users.noreply.github.com>
2022-10-10 07:35:05 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("svg").click(function() {
+    alert("I'm sorry, We do not have any social medias at the moment!")
+  });
+});
+
+$(function() {
+  $("#nn").click(function() {
+    alert("| News Zone - Delivering the latest news to you since 2019 |")
+  });
+});
+
+$(function() {
+  $("#sendoffer").click(function() {
+    alert("Thank you for contacting us, we will try to reply to you as fast as possible!")
+  });
+});
+
